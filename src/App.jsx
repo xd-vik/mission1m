689dbccc8f2b559ee12bc93ac77ba1371c851db0
Vivek <vivekcodzz@gmail.com>
2024-10-24 20:13:37 +0530

    Proj-Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold">
+        Mission One Million ! Project Setup
+      </h1>
+    </>
+  );
+}
+
+export default App;
