0c1d2b5ec58ed86dd4ca7b620ff80313d713c1e5
ManishNagar001 <mnagargujjar33@gmail.con>
2024-11-06 13:09:23 +0530

    Final Nav

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="rounded-xl  md:border-[3px] md:border-[#a29e9e] mx-1 md:mx-20 bg-green-100 h-[70px]">
-      <div className="max-w-7xl mx-auto px-1 flex flex-col md:flex-row justify-between items-center ">
+    <nav className="rounded-xl p-1 md:border-[3px] md:border-[#a29e9e] mx-1 md:mx-20 bg-green-100">
+      <div className="max-w-7xl mx-auto px-5 flex flex-col md:flex-row justify-between items-center ">
         {/* Left Section: Logo */}
-        <div className="flex  justify-between ml-[-250px] w-full">
-          <div className="pt-1">
+        <div className="flex  justify-between ml-[-270px] w-full">
+          <div className="">
             <img src={logo} alt="Logo" />
           </div>
 
@@ -27,19 +27,11 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* Main Menu Items */}
-<<<<<<< HEAD
+        {/* Main Menu*/}
         <div className="md:hidden ml-[-90%] mt-[-50px]">
             <img src={logo} alt="Logo" />
           </div>
         <div className={`flex-col md:flex md:flex-row ${isOpen ? "flex" : "hidden"} md:flex md:space-x-10 text-2xl mr-[400px]`}>
-=======
-        <div
-          className={`flex-col md:flex md:flex-row ${
-            isOpen ? "flex" : "hidden"
-          } md:flex md:space-x-10 text-2xl mr-[400px]`}
-        >
->>>>>>> 52edc8a446f8140eb99dba6f69c829e4264d0ca7
           <a
             href="#"
             className="text-black px-3 py-2 rounded-md font-medium hover:bg-green-200 text-xl "
@@ -50,7 +42,7 @@ const Navbar = () => {
             href="#"
             className="px-5 py-2 rounded-md font-medium hover:bg-green-200 text-xl w-[150px] "
           >
-            All Courses
+          All Courses
           </a>
           <a
             href="#"
@@ -66,21 +58,16 @@ const Navbar = () => {
           </a>
         </div>
 
-        {/* Right Section: Sign In and Sign Up */}
-        <div className="hidden md:flex space-x-7 mt-4 md:mt-0">
+        {/*  Sign In and Sign Up */}
+        <div className="hidden md:flex space-x-7 mt-4 md:mt-0 ">
           <a
-<<<<<<< HEAD
             href="#"
             className="text-black px-2 py-3 rounded-md font-medium hover:bg-green-200 text-xl  w-[80px] "
-=======
-            href="/Login"
-            className="text-black px-2 py-3 rounded-md font-medium hover:bg-green-200 text-xl  w-[90px]"
->>>>>>> 52edc8a446f8140eb99dba6f69c829e4264d0ca7
           >
             Sign In
           </a>
           <a
-            href="/Signup"
+            href="#"
             className="text-black px-2 py-3 rounded-md font-medium hover:bg-green-200 text-xl  w-[90px]"
           >
             Sign Up
@@ -88,11 +75,8 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div
-        className={`md:hidden ${
-          isOpen ? "flex" : "hidden"
-        } flex-col space-y-3 mt-4`}
-      >
+    
+      <div className={`md:hidden ${isOpen ? "flex" : "hidden"} flex-col space-y-3 mt-4`}>
         <a
           href="#"
           className="text-black px-2 py-3 rounded-md font-medium hover:bg-green-200 text-xl ml-20"
