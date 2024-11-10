4827af5b9bec03bc002dd143b641f1428ad7155c
ManishNagar001 <mnagargujjar33@gmail.con>
2024-11-10 19:12:55 +0530

    Final Nav

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -10,28 +10,31 @@ const Navbar = () => {
 
   return (
     <nav className="rounded-xl p-1 md:border-[3px] md:border-[#a29e9e] mx-1 md:mx-20 bg-green-100">
-      <div className="max-w-7xl mx-auto px-5 flex flex-col md:flex-row justify-between items-center ">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row  items-center ">
         {/* Left Section: Logo */}
-        <div className="flex  justify-between ml-[-270px] w-full">
-          <div className="">
-            <img src={logo} alt="Logo" />
+        <div className="  flex  justify-start ml-[-90%] mb-[60px]  md:ml-20 md:mb-0">
+          <div className="ml-0 md:ml-[-250px]">
+            <img src={logo} alt="Logo" className="md:ml-[100%]" />
           </div>
-
-
-          {/* Toggle Button for Mobile */}
-          <button
-            onClick={toggleMenu}
-            className="md:hidden text-black text-xl focus:outline-none mt-[1px] mr-[-100px] "
-          >
-            {isOpen ? "Close" : "Menu"}
-          </button>
         </div>
 
+        {/* Toggle Button for Mobile */}
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-black text-xl focus:outline-none mt-[-100px] mr-[-80%] "
+        >
+          {isOpen ? "Close" : "Menu"}
+        </button>
+
         {/* Main Menu*/}
-        <div className="md:hidden ml-[-90%] mt-[-50px]">
+        {/* <div className="md:hidden ml-[-90%] mt-[-5%]">
             <img src={logo} alt="Logo" />
-          </div>
-        <div className={`flex-col md:flex md:flex-row ${isOpen ? "flex" : "hidden"} md:flex md:space-x-10 text-2xl mr-[400px]`}>
+          </div> */}
+        <div
+          className={`flex-col md:flex md:flex-row ${
+            isOpen ? "flex" : "hidden"
+          } md:flex md:space-x-10 text-2xl mr-[70%] md:mr-[25%]`}
+        >
           <a
             href="#"
             className="text-black px-3 py-2 rounded-md font-medium hover:bg-green-200 text-xl "
@@ -40,9 +43,9 @@ const Navbar = () => {
           </a>
           <a
             href="#"
-            className="px-5 py-2 rounded-md font-medium hover:bg-green-200 text-xl w-[150px] "
+            className="px-3 py-2 rounded-md font-medium hover:bg-green-200 text-xl w-[145px] "
           >
-          All Courses
+            All Courses
           </a>
           <a
             href="#"
@@ -75,8 +78,11 @@ const Navbar = () => {
         </div>
       </div>
 
-    
-      <div className={`md:hidden ${isOpen ? "flex" : "hidden"} flex-col space-y-3 mt-4`}>
+      <div
+        className={`md:hidden ${
+          isOpen ? "flex" : "hidden"
+        } flex-col space-y-3 mt-4 ml-[-10%]`}
+      >
         <a
           href="#"
           className="text-black px-2 py-3 rounded-md font-medium hover:bg-green-200 text-xl ml-20"
